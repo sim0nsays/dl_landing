9ee4a1e0c90158278012fe43bc083bcad9d56929
Mike <acidmk2@gmail.com>
2018-12-29 20:32:29 +0700

    mailchimp form

--- scripts.js
+++ scripts.js
@@ -11,4 +11,16 @@ $( document ).ready(function() {
     $('.util-wrapper').css('display', 'flex');
     $('.util-wrapper').css('align-items', 'center');
     $('.util-wrapper').css('height', '155px');
+
+    $('.intro-join__btn').click(function() {
+        window.dojoRequire(["mojo/signup-forms/Loader"], function(L) {
+            // var creds = {"baseUrl":"mc.us7.list-manage.com","uuid":"a320e1b7c8bb271f1153b18f3","lid":"418af07ec4","uniqueMethods":true};
+            var creds = {"baseUrl":"mc.us7.list-manage.com","uuid":"583de279935b6df8bac8fa2cf","lid":"f93d72837f","uniqueMethods":true};
+            L.start(creds);
+            document.cookie = 'MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+        });
+    });
 });
+
+
+
