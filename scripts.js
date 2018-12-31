78a93fa7a05d7314499618c5b8cbeec30ed8cf80
Mike <acidmk2@gmail.com>
2018-12-31 23:17:52 +0700

    subscribe form added

--- scripts.js
+++ scripts.js
@@ -12,16 +12,7 @@ $(document).ready(function () {
     $('.util-wrapper').css('align-items', 'center');
     $('.util-wrapper').css('height', '115px');
 
-    $('.intro-join__btn').click(function (e) {
-        window.dojoRequire(["mojo/signup-forms/Loader"], function (L) {
-            var creds = {
-                "baseUrl": "mc.us7.list-manage.com",
-                "uuid": "583de279935b6df8bac8fa2cf",
-                "lid": "f93d72837f",
-                "uniqueMethods": true
-            };
-            L.start(creds);
-            document.cookie = 'MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
-        });
+    $('.intro-join__btn').click(function () {
+        $('#subscribeModal').modal({'backdrop': 'static'});
     });
 });
