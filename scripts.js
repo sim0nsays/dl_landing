b141a41925eb9c8caa4fa563d8fe82e1eded84fd
Unknown <acidmk2@gmail.com>
2019-02-14 23:48:20 +0700

    future lections stub

--- scripts.js
+++ scripts.js
@@ -13,11 +13,14 @@ $('#logo-showcase-gray').utilCarousel({
 // var firstScriptTag = document.getElementsByTagName('script')[0];
 // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+var FUTURE_VIDEO_ID = null;
+
 var docs = {
     videos: [
-        { id: 'riLQCudri7Q', title: 'Лекция 1 - Введение', description: '10.02.2007'},
-        { id: 'ZNDbcxLDCOs', title: 'Лекция 2 - Линейный классификатор', description: '10.02.2007'},
-        { id: 'ZNDbcxLDCOs', title: 'Лекция 2 - Линейный классификатор', description: '10.02.2007'},
+        { id: FUTURE_VIDEO_ID, title: 'Лекция 4 - Введение', description: '10.02.2007', date: '10.02.2007'},
+        { id: 'ZNDbcxLDCOs', title: 'Лекция 3 - Линейный классификатор', description: '10.02.2007', date: '10.02.2007'},
+        { id: 'ZNDbcxLDCOs', title: 'Лекция 2 - Линейный классификатор', description: '10.02.2007', date: '10.02.2007'},
+        { id: 'riLQCudri7Q', title: 'Лекция 1 - Введение', description: '10.02.2007', date: '10.02.2007'}
     ]
 };
 
@@ -53,18 +56,37 @@ $(document).ready(function () {
     var container1 = $('#simpleImg');
 
     docs.videos.forEach(function (v) {
+        var mediaTmpl;
+
+        if (v.id === FUTURE_VIDEO_ID) {
+            mediaTmpl =
+                '<div class="meida-holder">' +
+                    '<img src="./assets/images/img_stub.jpg" height="240" width="320" alt="" />' +
+                    '<div class="hover-content">' +
+                        '<div class="overlay"></div>' +
+                    '</div>' +
+                    '<div class="link-container" style="color: #fff">' +
+                        '<div>Следующая лекция</div>' +
+                        '<div>' + v.date + '</div>' +
+                    '</div>' +
+                '</div>';
+        } else {
+            mediaTmpl =
+            '<div class="meida-holder">' +
+            '<img src="https://img.youtube.com/vi/' + v.id + '/0.jpg" alt="" />' +
+            '<div class="hover-content">' +
+                '<div class="overlay"></div>' +
+                '<div class="link-container">' +
+                    '<a target="_blank" href="' + link + '"><i class="icon-link"></i></a><a href="' + link + '" class="video-link" ><i class="icon-play"></i></a>' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+        }
+
         var link = 'http://www.youtube.com/watch?v=' + v.id;
         var tmpl =
             '<div class="item">' +
-                '<div class="meida-holder">' +
-                    '<img src="https://img.youtube.com/vi/' + v.id + '/0.jpg" alt="" />' +
-                    '<div class="hover-content">' +
-                        '<div class="overlay"></div>' +
-                        '<div class="link-container">' +
-                            '<a target="_blank" href="' + link + '"><i class="icon-link"></i></a><a href="' + link + '" class="video-link" ><i class="icon-play"></i></a>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>' +
+                mediaTmpl +
                 '<div class="detail-container">' +
                     '<h4>' + v.title + '</h4>' +
                     '<p>' +
