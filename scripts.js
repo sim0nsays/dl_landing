21e11fe11fd72aebce491b45fd904942ccf69127
Unknown <acidmk2@gmail.com>
2019-02-16 23:56:41 +0700

    tabs

--- scripts.js
+++ scripts.js
@@ -71,19 +71,20 @@ $(document).ready(function () {
                     '</div>' +
                 '</div>';
         } else {
+            var link = 'http://www.youtube.com/watch?v=' + v.id;
             mediaTmpl =
             '<div class="meida-holder">' +
-            '<img src="https://img.youtube.com/vi/' + v.id + '/0.jpg" alt="" />' +
-            '<div class="hover-content">' +
-                '<div class="overlay"></div>' +
-                '<div class="link-container">' +
-                    '<a target="_blank" href="' + link + '"><i class="icon-link"></i></a><a href="' + link + '" class="video-link" ><i class="icon-play"></i></a>' +
+                '<img src="https://img.youtube.com/vi/' + v.id + '/0.jpg" alt="" />' +
+                '<div class="hover-content">' +
+                    '<div class="overlay"></div>' +
+                    '<div class="link-container">' +
+                        '<a target="_blank" href="' + link + '"><i class="icon-link"></i></a><a href="' + link + '" class="video-link" ><i class="icon-play"></i></a>' +
+                    '</div>' +
                 '</div>' +
-            '</div>' +
-        '</div>';
+            '</div>';
         }
 
-        var link = 'http://www.youtube.com/watch?v=' + v.id;
+        
         var tmpl =
             '<div class="item">' +
                 mediaTmpl +
@@ -107,3 +108,79 @@ $(document).ready(function () {
     });
 
 });
+
+// TOGGLE SECTIONS
+// querySelector, jQuery style
+// var $ = function (selector) {
+//     return document.querySelectorAll(selector);
+//     };
+    
+    
+    // Define tabs, write down them classes
+    var tabs = [
+        '.tabbed-section__selector-tab-1',
+        '.tabbed-section__selector-tab-2',
+        '.tabbed-section__selector-tab-3'
+    ];
+    
+    // Create the toggle function
+    var toggleTab = function(element) {
+    var parent = element.parentNode;
+    
+    // Do things on click
+    $(element)[0].addEventListener('click', function(e){
+        e.preventDefault();
+    
+        // Remove the active class on all tabs.
+        // climbing up the DOM tree with `parentNode` and target 
+        // the children ( the tabs ) with childNodes
+        this.parentNode.childNodes[1].classList.remove('active');
+        this.parentNode.childNodes[3].classList.remove('active');
+        this.parentNode.childNodes[5].classList.remove('active');
+    
+        // Then, give `this` (the clicked tab), the active class
+        this.classList.add('active');
+        
+        // Check if the clicked tab contains the class of the 1 or 2
+        if(this.classList.contains('tabbed-section__selector-tab-1')) {
+        // and change the classes, show the first content panel
+        $('.tabbed-section-1')[0].classList.remove('hidden');
+        $('.tabbed-section-1')[0].classList.add('visible');
+        
+        // Hide the second
+        $('.tabbed-section-2')[0].classList.remove('visible');
+        $('.tabbed-section-2')[0].classList.add('hidden');
+            $('.tabbed-section-3')[0].classList.remove('visible');
+        $('.tabbed-section-3')[0].classList.add('hidden');
+        }
+    
+        if(this.classList.contains('tabbed-section__selector-tab-2')) {
+        // and change the classes, show the second content panel
+        $('.tabbed-section-2')[0].classList.remove('hidden');
+        $('.tabbed-section-2')[0].classList.add('visible');
+        // Hide the first
+        $('.tabbed-section-1')[0].classList.remove('visible');
+        $('.tabbed-section-1')[0].classList.add('hidden');
+        $('.tabbed-section-3')[0].classList.remove('visible');
+        $('.tabbed-section-3')[0].classList.add('hidden');
+        }
+        
+        if(this.classList.contains('tabbed-section__selector-tab-3')) {
+        // and change the classes, show the second content panel
+        $('.tabbed-section-3')[0].classList.remove('hidden');
+        $('.tabbed-section-3')[0].classList.add('visible');
+        // Hide the first
+        $('.tabbed-section-1')[0].classList.remove('visible');
+        $('.tabbed-section-1')[0].classList.add('hidden');
+        $('.tabbed-section-2')[0].classList.remove('visible');
+        $('.tabbed-section-2')[0].classList.add('hidden');
+        }
+    });
+    };
+    
+    // Then finally, iterates through all tabs, to activate the 
+    // tabs system.
+    for (var i = tabs.length - 1; i >= 0; i--) {
+    toggleTab(tabs[i])
+    };
+    
